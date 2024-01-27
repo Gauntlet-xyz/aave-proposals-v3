eb614be87fc47d1bef100750bb8d4f94fb65a4c0
Jeremy Batchelder <jpbatch3@gmail.com>
2024-01-26 16:06:24 -0800

    Accept CLI

--- gauntletTooling/index.ts
+++ gauntletTooling/index.ts
@@ -1,39 +1,47 @@
 import * as fs from 'fs';
 import {transformInput, InputObject} from './transformInput';
 
-async function processInputFile(filePath: string) {
-  fs.readFile(filePath, {encoding: 'utf-8'}, async (err, data) => {
+async function processInputData(inputData: string) {
+  try {
+    const inputObject: InputObject = JSON.parse(inputData);
+    const transformedObject = await transformInput(inputObject);
+
+    const outputString = `import { ConfigFile } from '../../generator/types';\nexport const config: ConfigFile = ${JSON.stringify(
+      transformedObject,
+      null,
+      2
+    )};`;
+    fs.writeFile('./gauntlet-tooling/config.ts', outputString, (writeErr) => {
+      if (writeErr) {
+        console.error('Error writing to file:', writeErr);
+      } else {
+        console.log('Successfully written to config.ts');
+      }
+    });
+  } catch (parseErr) {
+    console.error('Error processing input:', parseErr);
+  }
+}
+
+function processInputFile(filePath: string) {
+  fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
     if (err) {
       console.error('Error reading file:', err);
       return;
     }
-
-    try {
-      const inputObject: InputObject = JSON.parse(data);
-      const transformedObject = await transformInput(inputObject);
-
-      const outputString = `import { ConfigFile } from '../../generator/types';\nexport const config: ConfigFile = ${JSON.stringify(
-        transformedObject,
-        null,
-        2
-      )};`;
-      fs.writeFile('./gauntlet-tooling/config.ts', outputString, (writeErr) => {
-        if (writeErr) {
-          console.error('Error writing to file:', writeErr);
-        } else {
-          console.log('Successfully written to config.ts');
-        }
-      });
-    } catch (parseErr) {
-      console.error('Error parsing JSON:', parseErr);
-    }
+    processInputData(data);
   });
 }
 
-const filePath = process.argv[2];
-if (!filePath) {
-  console.error('Please provide a file path as an argument.');
+const input = process.argv[2];
+if (!input) {
+  console.error('Please provide a file path or JSON string as an argument.');
   process.exit(1);
 }
 
-processInputFile(filePath);
+// Check if the input is a file path or a JSON string
+if (fs.existsSync(input)) {
+  processInputFile(input);
+} else {
+  processInputData(input);
+}
